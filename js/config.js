bfc67d4dd929c80fba4e5f12ee7c88c54b9eb8a0
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2017-04-15 16:31:54 -0300

    adicionado aplicação vuejs

---
+++ js/config.js
@@ -0,0 +1,6 @@
+new Vue({
+  el: '#app', // O id do elemento que quero controlar
+  data: {
+    title: 'Hello World!' // O dado que irei colocar na variável 
+  }                       // que estará no meu elemento
+});
