f65c3dfa260f91fca4b53a7c1a18647db17b66db
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2017-04-15 16:45:27 -0300

    Utilizando input com vuejs

--- js/config.js
+++ js/config.js
@@ -2,5 +2,10 @@ new Vue({
   el: '#app', // O id do elemento que quero controlar
   data: {
     title: 'Hello World!' // O dado que irei colocar na variável 
-  }                       // que estará no meu elemento
+  },                      // que estará no meu elemento
+  methods: {
+    changeTitle: function(event){
+      this.title = event.target.value;
+    }
+  }
 });
